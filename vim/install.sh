fb29174e94a932a90598eb8fb512c00392635d82
Blizzard57 <kalp77000@gmail.com>
2019-03-04 18:50:21 +0530

    Small Changes

--- vim/install.sh
+++ vim/install.sh
@@ -5,5 +5,6 @@ echo "Installing Git"
 sudo apt install git
 echo "Installing Vundle"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+sudo apt-get install fonts-powerline
 echo "Installing the plugins"
 vim +PluginInstall +qall
