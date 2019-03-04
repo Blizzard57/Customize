cafbcd736512c3c8f9772111ab554e4e96472d12
Blizzard57 <kalp77000@gmail.com>
2019-03-04 18:41:29 +0530

    Commits

---
+++ dmenu/install.sh
@@ -0,0 +1,3 @@
+sudo apt install dmenu
+dmenu_run -nb black -nf white -sb orange -fn lato
+echo "The following command is to be keybinded to Super+Shift+S : dmenu_run -nb black -nf white -sb orange -fn lato"
