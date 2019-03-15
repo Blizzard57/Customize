9b5e7d32e4a9779906722042dfc89240acba4cf3
Blizzard57 <kalp77000@gmail.com>
2019-03-15 22:14:20 +0530

    Small Changes

---
+++ install.sh
@@ -0,0 +1,3 @@
+sudo apt update && sudo apt upgrade
+sudo apt add-apt-repository ppa:snwh/ppa
+sudo apt install gnome-tweak-tool jupyter-notebook zsh paper-icon-theme vim variety git dmenu x11-utils dconf-editor fonts-powerline
